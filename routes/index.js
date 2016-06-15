d18fbb334519b5c9540d58649d933f7efefda769
Roberto Ortega <berto.ort@gmail.com>
2016-06-15 16:47:58 -0600

    fully crudded

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,49 @@
 var express = require('express');
 var router = express.Router();
+var knex = require('../db/knex');
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  knex('amphibians').select().then(function (resultsFromQuery) {
+    res.render('index', {list: resultsFromQuery});
+  })
 });
+
+router.get('/create', function (req, res, next) {
+  res.render('create')
+});
+
+router.get('/:id/edit', function (req, res, next) {
+  knex('amphibians').where({id: req.params.id}).first().then(function (frog) {
+    res.render('edit', {frog: frog})
+  })
+})
+
+router.post('/:id/edit', function (req, res, next) {
+  knex('amphibians').where({id: req.params.id}).update(req.body).then(function () {
+    res.redirect("/" + req.params.id)
+  })
+})
+
+router.get('/:id/delete', function (req, res, next) {
+  knex('amphibians').where({id: req.params.id}).del().then(function () {
+    res.redirect('/')
+  })
+})
+
+router.get('/:id', function (req, res, next) {
+  knex('amphibians').where({id: req.params.id}).first().then(function (froggy) {
+    res.render('detail', {froggy: froggy})
+  })
+});
+
+router.post('/create', function (req, res, next) {
+  knex('amphibians').insert(req.body).then(function () {
+    res.redirect('/');
+  }).catch(function (err) {
+    console.log(err);
+    next(err)
+  })
+})
+
 
 module.exports = router;
